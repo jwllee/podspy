5591b49c029fdbb185eb42bac57ce524b1524292
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-04-15 09:41:48 -0300

    Added simple test

--- tests/log/test_xes2pd.py
+++ tests/log/test_xes2pd.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
-"""This is the example module.
+"""This is the test module for xes2pd module.
 
-This module does stuff.
 """
+
+
+import pytest
+
 
 __author__ = "Wai Lam Jonathan Lee"
 __email__ = "walee@uc.cl"
 
+
+def test_simple():
+    assert 1 + 1 == 2
