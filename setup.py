80c2be17011660d494f33d4ca59450c305feb61d
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-04-15 09:40:33 -0300

    Setup for package installation

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-"""This is the example module.
 
-This module does stuff.
-"""
+from glob import glob
+from os.path import splitext, basename
+from setuptools import find_packages, setup
+
 
 __author__ = "Wai Lam Jonathan Lee"
 __email__ = "walee@uc.cl"
 
+
+setup(
+    name='podspy',
+    version='0.1.0',
+    description='A SciKit for process oriented data science',
+    author='Wai Lam Jonathan Lee',
+    author_email='walee@uc.cl',
+    install_requires=['numpy', 'pandas', 'opyenxes'],
+    tests_require=['pytest'],
+    entry_points={'pytest11': ['podspy = podspy']},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]
+)
