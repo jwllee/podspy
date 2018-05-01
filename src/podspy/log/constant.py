ea20a845562704d2092af73b124be018a82a2eef
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-05-01 17:40:24 -0300

    Moved constants to separate module

---
+++ src/podspy/log/constant.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""This is the constant module.
+
+This module contains constant values.
+"""
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+DISCRETE = 'DISCRETE'
+LITERAL = 'LITERAL'
+CONTINUOUS = 'CONTINUOUS'
+BOOLEAN = 'BOOLEAN'
+TIMESTAMP = 'TIMESTAMP'
+
+CASEID = 'caseid'
+ACTIVITY = 'activity'
+
+
