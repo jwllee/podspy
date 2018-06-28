8a17d4eaf8f2ca947ebc74ed995c3262a63b9780
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-06-28 16:11:07 -0400

    Moved constants to constants module

--- src/podspy/log/constant.py
+++ src/podspy/log/constant.py
@@ -17,5 +17,11 @@ TIMESTAMP = 'TIMESTAMP'
 
 CASEID = 'caseid'
 ACTIVITY = 'activity'
+VARIANT = 'variant'
 
-
+CONCEPT_NAME = 'concept:name'
+COST_AMOUNT = 'cost:amount'
+COST_TOTAL = 'cost:total'
+LIFECYCLE_TRANS = 'lifecycle:transition'
+ORG_GROUP = 'org:group'
+TIME_TIMESTAMP = 'time:timestamp'
