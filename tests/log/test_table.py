c6327dabdc643601b6e0ca1e69f7cc5b8008a29b
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-06-28 17:32:53 -0400

    Unit tests for get trace variants function

--- tests/log/test_table.py
+++ tests/log/test_table.py
@@ -70,3 +70,45 @@ def test_get_event_identity_list(a_log_table, an_event_clf_and_event_identity_li
     expected = an_event_clf_and_event_identity_list[1]
     computed = a_log_table.get_event_identity_list(clf_name)
     assert computed == expected
+
+
+def test_get_trace_variants_no_activity_column_raises_value_error(a_log_table):
+    with pytest.raises(ValueError):
+        a_log_table.get_trace_variants()
+
+
+def test_get_trace_variants_no_caseid_column_raises_value_error(a_log_table):
+    with pytest.raises(ValueError):
+        a_log_table.get_trace_variants()
+
+
+def test_get_trace_variants_two_cases_one_variant():
+    log_table = LogTable()
+    variant = 'a{}b{}c'.format(LogTable.VARIANT_SEP, LogTable.VARIANT_SEP)
+    events = ['a', 'b', 'c']
+    log_table.event_df[const.ACTIVITY] = events * 2
+    log_table.event_df[const.CASEID] = ['0', '0', '0', '1', '1', '1']
+    variant_df = log_table.get_trace_variants()
+
+    expected = pd.DataFrame({const.CASEID: ['0', '1'],
+                             LogTable.VARIANT_ID: ['variant 0', 'variant 0'],
+                             const.VARIANT: [variant, variant]})
+    expected = expected[[const.CASEID, LogTable.VARIANT_ID, const.VARIANT]]
+
+    assert_frame_equal(variant_df, expected)
+
+def test_get_trace_variants_two_cases_two_variants():
+    log_table = LogTable()
+    variant0 = 'a{}b{}c'.format(LogTable.VARIANT_SEP, LogTable.VARIANT_SEP)
+    variant1 = 'a{}b{}d'.format(LogTable.VARIANT_SEP, LogTable.VARIANT_SEP)
+    events = ['a', 'b', 'c', 'a', 'b', 'd']
+    log_table.event_df[const.ACTIVITY] = events
+    log_table.event_df[const.CASEID] = ['0', '0', '0', '1', '1', '1']
+    variant_df = log_table.get_trace_variants()
+
+    expected = pd.DataFrame({const.CASEID: ['0', '1'],
+                             LogTable.VARIANT_ID: ['variant 0', 'variant 1'],
+                             const.VARIANT: [variant0, variant1]})
+    expected = expected[[const.CASEID, LogTable.VARIANT_ID, const.VARIANT]]
+
+    assert_frame_equal(variant_df, expected)
