f7fa1bdf3469b4a5d3ed2346dca41839a984e939
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-07-16 16:31:57 -0400

    Pattern module

---
+++ tests/util/test_pattern.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""This is the example module.
+
+This module does stuff.
+"""
+
+from podspy.util.pattern import Singleton
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+class MockSingleton(Singleton):
+    def __init__(self, var='hello world'):
+        self.var = var
+
+
+def test_singleton_is_single():
+    s0 = MockSingleton()
+    s1 = MockSingleton()
+
+    assert id(s0) == id(s1)
+    assert s0 is s1
+
+    # changing the variable of s0 should change s1's as well
+    s0.var = 'bye'
+
+    assert s0 is s1
+    assert s0.var == s1.var
