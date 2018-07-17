5163edccc40ae98b9ec8cd29a2b2437d4682f40d
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-07-16 20:00:24 -0400

    Graph base module

---
+++ tests/graph/test_base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""This is the unit test module.
+
+This module tests classes in the base module.
+"""
+
+
+from podspy.graph.base import AbstractGraphEdge, AbstractGraph
+
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+class MockAbstractGraphEdge(AbstractGraphEdge):
+    def __init__(self, src, target, *args, **kwargs):
+        super().__init__(src, target, *args, **kwargs)
+
+
+class TestAbstractGraphEdge:
+    def test_edge_equality(self):
+        e0 = MockAbstractGraphEdge('a', 'b')
+        e1 = MockAbstractGraphEdge('a', 'b')
+
+        assert e0 == e1
+
+        e2 = MockAbstractGraphEdge('a', 'c')
+
+        assert e0 != e2
