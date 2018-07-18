c164b9d85f4c4bbe91b07dcaf015e9a5c5894032
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-07-18 12:39:48 -0400

    Pnml io module

---
+++ src/podspy/petrinet/pnml/io.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""This is the example module.
+
+This module does stuff.
+"""
+
+
+from lxml import etree
+import logging
+
+from podspy.petrinet.pnml.factory import *
+from podspy.petrinet.pnml.element import *
+from podspy.petrinet.pnml.base import *
+from podspy.petrinet.pnml.extension import *
+
+from podspy.petrinet.factory import *
+from podspy.petrinet.semantics import *
+
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+logger = logging.getLogger(__file__)
+
+
+def make_from_element(element, parent_cls):
+    pnml = PnmlFactory.create_pnml(element.tag, parent_cls)
+
+    # logger.debug('Making {!r} into {!r}'.format(element, pnml.__class__.__name__))
+
+    pnml.add_attrib(element.attrib)
+
+    # logger.debug('Adding {} as text to {}'.format(element.text, pnml))
+    pnml.add_text(element.text)
+    # if isinstance(pnml, PnmlText):
+        # logger.debug('After adding {} as text: {}'.format(element.text, pnml))
+
+    for c in element.getchildren():
+        # recursively call make from element on the child element
+        pnml_child = make_from_element(c, pnml.__class__)
+
+        if isinstance(pnml, PnmlPlace):
+            logger.debug('Adding {!r} as child of {!r}'.format(c, pnml.name))
+        # if isinstance(pnml, PnmlNet):
+        #     logger.debug('Adding {!r} as child of {!r}'.format(pnml_child, pnml))
+        # if isinstance(pnml, PnmlName):
+        #     logger.debug('Adding {!r} as child of {!r}'.format(pnml_child, pnml))
+
+        pnml.add_child(pnml_child)
+
+    return pnml
+
+
+def import_pnml_from_file(filename):
+    with open(filename, 'r') as f:
+        tree = etree.parse(f)
+
+    root = tree.getroot()
+
+    # should be <pnml>
+    assert root.tag == Pnml.TAG, 'Root tag does not equal {}, not a pnml file'.format(Pnml.TAG)
+
+    pnml = make_from_element(root, None)
+
+    return pnml
+
+
+def pnml_to_petrinet(pnml):
+    pnml_net = pnml.net_list[0]
+    net = PetrinetFactory.new_petrinet(pnml_net.name)
+    marking = Marking()
+    final_markings = set()
+    pnml.convert_to_net(net, marking, final_markings)
+    return net, marking, final_markings
