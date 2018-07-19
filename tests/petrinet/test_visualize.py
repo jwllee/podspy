ad31e6d0bec9f0a18d6f12e5efb69ca898ecc333
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-07-19 09:44:28 -0400

    Unit tests for visualize module

---
+++ tests/petrinet/test_visualize.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""This is the unit test module.
+
+This module tests the visualize module.
+"""
+
+
+import os
+from podspy.petrinet.pnml import io
+from podspy.petrinet import visualize as vis
+import pygraphviz as pgv
+
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+def test_ptnet2dot():
+    net_file = os.path.join('.', 'tests', 'testdata', 'simple.pnml')
+    pnml = io.import_pnml_from_file(net_file)
+    ptnet, marking, final_markings = io.pnml_to_petrinet(pnml)
+
+    G = vis.net2dot(ptnet, marking, layout='dot')
+    # uncomment to draw out the figure
+    # G.draw('./simple-with-marking.png')
+
+    assert isinstance(G, pgv.AGraph)
+
+    G = vis.net2dot(ptnet, layout='dot')
+    # same as above
+    # G.draw('./simple.png')
+
+    assert isinstance(G, pgv.AGraph)
+
