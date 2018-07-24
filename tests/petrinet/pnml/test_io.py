9dbcac82be1d6ea36434f02cb23103567fce0603
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-07-24 10:04:42 -0400

    Tests for ptnet2pnml conversion and pnml file export

--- tests/petrinet/pnml/test_io.py
+++ tests/petrinet/pnml/test_io.py
@@ -5,12 +5,14 @@
 This module tests pnml io module.
 """
 
-import os, logging
+import os, logging, pytest
+from io import StringIO
 
 from podspy.petrinet.pnml import io
 from podspy.petrinet.net import *
 from podspy.petrinet.pnml.element import *
 from podspy.petrinet.semantics import *
+import podspy.petrinet.visualize as vis
 
 
 __author__ = "Wai Lam Jonathan Lee"
@@ -20,18 +22,25 @@ __email__ = "walee@uc.cl"
 logger = logging.getLogger('tests')
 
 
+@pytest.fixture
+def net_fp():
+    return os.path.join('.', 'tests', 'testdata', 'net1.pnml')
+
+
 def test_import_pnml():
     net_fp = os.path.join('.', 'tests', 'testdata', 'net1.pnml')
-    f = open(net_fp, 'r')
-    pnml = io.import_pnml_from_file(f)
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
     assert pnml is not None
     assert isinstance(pnml, Pnml)
 
 
 def test_import_petrinet():
     net_fp = os.path.join('.', 'tests', 'testdata', 'simple.pnml')
-    f = open(net_fp, 'r')
-    pnml = io.import_pnml_from_file(f)
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
     net, marking, final_markings = io.pnml2ptnet(pnml)
 
     assert isinstance(net, Petrinet)
@@ -61,3 +70,75 @@ def test_import_petrinet():
     assert 'n1' in place_labels and \
            'n2' in place_labels and \
            'n3' in place_labels
+
+
+def test_ptnet2pnml_without_layout(net_fp):
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
+    apnet = AcceptingPetrinet(*io.pnml2ptnet(pnml))
+
+    converted_pnml = io.apnet2pnml(apnet)
+
+    assert isinstance(converted_pnml, Pnml)
+    assert pnml == converted_pnml
+
+
+def test_ptnet2pnml_with_layout(net_fp):
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
+    apnet = AcceptingPetrinet(*io.pnml2ptnet(pnml))
+
+    G = vis.net2dot(apnet.net, apnet.init_marking, layout='dot')
+    converted_pnml = io.apnet2pnml(apnet, G)
+
+    assert isinstance(converted_pnml, Pnml)
+    assert pnml == converted_pnml
+
+
+def test_export_petrinet_without_layout(net_fp):
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
+    net, marking, final_markings = io.pnml2ptnet(pnml)
+
+    assert isinstance(marking, Marking)
+
+    apnet = AcceptingPetrinet(net, marking, final_markings)
+
+    out_f = StringIO('out_f')
+
+    io.export_pnml_to_file(apnet, out_f)
+
+    with open(net_fp, 'r') as f:
+        expected = f.read()
+
+    with open('./parsed.pnml', 'w') as f:
+        io.export_pnml_to_file(apnet, f)
+
+    assert expected == out_f.getvalue()
+
+
+def test_export_petrinet_with_layout(net_fp):
+    with open(net_fp, 'r') as f:
+        pnml = io.import_pnml_from_file(f)
+
+    net, marking, final_markings = io.pnml2ptnet(pnml)
+    G = vis.net2dot(net, marking, layout='dot')
+
+    assert isinstance(marking, Marking)
+
+    apnet = AcceptingPetrinet(net, marking, final_markings)
+
+    out_f = StringIO('out_f')
+
+    io.export_pnml_to_file(apnet, out_f, layout=G)
+
+    with open(net_fp, 'r') as f:
+        expected = f.read()
+
+    # with open('./parsed.pnml', 'w') as f:
+    #     io.export_pnml_to_file(apnet, f, layout=G)
+
+    assert expected == out_f.getvalue()
