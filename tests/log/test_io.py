cc9e8d3e1283c3bd2a08c18d1cf8b3fad5d39f1a
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-08-04 14:18:46 -0400

    Unit test for parse_extension_elem

--- tests/log/test_io.py
+++ tests/log/test_io.py
@@ -9,6 +9,7 @@ This module tests the log io module.
 from podspy.log import io
 import pytest
 from lxml import etree
+from urllib.parse import urlparse
 
 
 __author__ = "Wai Lam Jonathan Lee"
@@ -43,3 +44,33 @@ def test_parse_classifier_elem(classifier_elem):
     assert d[expected_name] == expected_clf_keys
 
 
+@pytest.fixture(ids=lambda s: s[1], params=[
+    ('<extension name="Concept" prefix="concept" uri="http://www.xes-standard.org/concept.xesext"/>',
+     'Concept', 'concept', urlparse('http://www.xes-standard.org/concept.xesext')),
+    ('<extension name="Lifecycle" prefix="lifecycle" uri="http://www.xes-standard.org/lifecycle.xesext"/>',
+     'Lifecycle', 'lifecycle', urlparse('http://www.xes-standard.org/lifecycle.xesext')),
+    ('<extension name="Organizational" prefix="org" uri="http://www.xes-standard.org/org.xesext"/>',
+     'Organizational', 'org', urlparse('http://www.xes-standard.org/org.xesext')),
+    ('<extension name="3TU metadata" prefix="meta_3TU" uri="http://www.xes-standard.org/meta_3TU.xesext"/>',
+     '3TU metadata', 'meta_3TU', urlparse('http://www.xes-standard.org/meta_3TU.xesext'))
+])
+def extension_elem(request):
+    return etree.fromstring(request.param[0]), request.param[1], request.param[2], request.param[3]
+
+
+def test_check_extension_elem_fixture(extension_elem):
+    assert extension_elem is not None
+
+
+def test_parse_extension_elem(extension_elem):
+    d = io.parse_extension_elem(extension_elem[0])
+
+    expected_name = extension_elem[1]
+    expected_prefix = extension_elem[2]
+    expected_uri = extension_elem[3]
+
+    assert expected_name in d
+    extension = d[expected_name]
+    assert extension[0] == expected_name
+    assert extension[1] == expected_prefix
+    assert extension[2].geturl() == expected_uri.geturl()
