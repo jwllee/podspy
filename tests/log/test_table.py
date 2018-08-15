21f5c5b445deb31cb22e3da9f460c08450401782
jonathan <jonathan.wailam.lee@gmail.com>
2018-08-15 14:04:36 -0300

    Removed XLogToLogTable class and related tests

--- tests/log/test_table.py
+++ tests/log/test_table.py
@@ -37,33 +37,6 @@ def factory():
     return XLogToLogTable()
 
 
-def test_xevents2df(an_event_df, a_xtrace, factory):
-    caseid = CONCEPT_EXTENSION.extract_name(a_xtrace)
-    caseid_list = [caseid for _ in range(len(a_xtrace))]
-    df = factory.xevents2df(caseid_list, a_xtrace)
-    expected = an_event_df[(an_event_df[const.CASEID] == caseid)]
-    expected.reset_index(inplace=True, drop=True)
-    assert_frame_equal(df, expected)
-
-
-def test_xtraces2df(a_trace_df, an_xlog, factory):
-    df = factory.xtraces2df(an_xlog)
-    assert_frame_equal(a_trace_df, df)
-
-
-def test_xlog2df(a_log_table, an_xlog, factory):
-    table = factory.xlog2table(an_xlog)
-    expected = a_log_table
-
-    assert table.attributes == expected.attributes
-    assert table.global_event_attributes == expected.global_event_attributes
-    assert table.global_trace_attributes == expected.global_trace_attributes
-    assert table.extensions == expected.extensions
-    assert table.classifiers == expected.classifiers
-    assert_frame_equal(table.event_df, expected.event_df)
-    assert_frame_equal(table.trace_df, expected.trace_df)
-
-
 def test_get_event_identity_list(a_log_table, an_event_clf_and_event_identity_list):
     # set the event classifier of the log table
     clf_name = an_event_clf_and_event_identity_list[0]
