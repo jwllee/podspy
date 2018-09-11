518a38e6790a9f92b397dcb786e9f09d3bc1d690
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-10 23:33:21 -0300

    Unit test for building footprint matrix from causal matrix

---
+++ tests/discovery/test_alpha.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""This is the unit test module for the alpha discovery module.
+
+"""
+
+
+import pytest
+import pandas as pd
+import numpy as np
+
+from podspy.discovery import alpha
+from podspy.structure import CausalMatrix
+
+
+@pytest.fixture()
+def simple_causal_matrix():
+    # causal matrix
+    mat = [
+        (0, 0, 83, 0, 0),   # a row
+        (0, 0, 64, 0, 0),   # b row
+        (0, 0, 0, 87, 60),  # c row
+        (0, 0, 0, 0, 0),    # d row
+        (0, 0, 0, 0, 0)
+    ]
+
+    mat = np.array(mat)
+    activity_list = ['a', 'b', 'c', 'd', 'e']
+    df = pd.DataFrame(mat, index=activity_list, columns=activity_list)
+    cmat = CausalMatrix(activity_list, df)
+
+    return cmat
+
+
+class TestFootprintMatrix:
+    def test_build_from_causal_matrix(self, simple_causal_matrix):
+        print(simple_causal_matrix)
+
+        footprint = alpha.FootprintMatrix.build_from_causal_matrix(simple_causal_matrix)
+
+        assert isinstance(footprint, alpha.FootprintMatrix)
+
+        # expected footprint matrix
+        # [
+        #     (#, #, ->, #, #)
+        #     (#, #, ->, #, #)
+        #     (<-, <-, #, ->, ->)
+        #     (#, #, <-, #, #)
+        #     (#, #, <-, #, #)
+        # ]
+
+        w = alpha.FootprintMatrix.NEVER_FOLLOW
+        x = alpha.FootprintMatrix.DIRECT_RIGHT
+        y = alpha.FootprintMatrix.DIRECT_LEFT
+        z = alpha.FootprintMatrix.PARALLEL
+
+        expected = [
+            (w, w, x, w, w),
+            (w, w, x, w, w),
+            (y, y, w, x, x),
+            (w, w, y, w, w),
+            (w, w, y, w, w)
+        ]
+
+        assert (footprint.matrix.values == expected).all()
+
+
+class TestAlphaMiner:
+    def test_alpha_simple_causal_matrix(self, simple_causal_matrix):
+        net = alpha.discover(simple_causal_matrix)
