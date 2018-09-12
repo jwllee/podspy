3e886b958dc8b4e3a86de2169439f514a18b0c3a
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-12 19:10:13 -0300

    Add test fixture

--- tests/discovery/test_alpha.py
+++ tests/discovery/test_alpha.py
@@ -6,10 +6,32 @@
 
 
 import pytest, os
+import pandas as pd
+import numpy as np
 
 from podspy.discovery import alpha
 from podspy.petrinet.nets import *
 from podspy.petrinet import visualize as vis
+from podspy.structure import CausalMatrix
+
+
+@pytest.fixture()
+def simple_causal_matrix():
+    # causal matrix
+    mat = [
+        (0, 0, 83, 0, 0),   # a row
+        (0, 0, 64, 0, 0),   # b row
+        (0, 0, 0, 87, 60),  # c row
+        (0, 0, 0, 0, 0),    # d row
+        (0, 0, 0, 0, 0)
+    ]
+
+    mat = np.array(mat)
+    activity_list = ['a', 'b', 'c', 'd', 'e']
+    df = pd.DataFrame(mat, index=activity_list, columns=activity_list)
+    cmat = CausalMatrix(activity_list, df)
+
+    return cmat
 
 
 class TestAlphaMiner:
