8e363bbb61d2d03895ebc2e5554bbe79fe592604
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-12 23:37:45 -0300

    Update unit test

--- tests/discovery/test_alpha.py
+++ tests/discovery/test_alpha.py
@@ -36,10 +36,56 @@ def simple_causal_matrix():
 
 class TestAlphaMiner:
     def test_alpha_simple_causal_matrix(self, simple_causal_matrix):
-        net = alpha.discover(simple_causal_matrix)
+        apn = alpha.classic.apply(simple_causal_matrix)
 
-        assert isinstance(net, AcceptingPetrinet)
+        assert isinstance(apn, AcceptingPetrinet)
 
         net_fp = os.path.join('.', 'tests', 'alpha.png')
-        G = vis.net2dot(net.net)
+        G = vis.net2dot(apn.net)
         # G.draw(net_fp)
+
+        net, init_marking, final_markings = apn
+        final_marking = final_markings.pop()
+
+        assert isinstance(net, Petrinet)
+
+        assert len(init_marking) == 1, \
+            'Initial marking contains {} places'.format(len(init_marking))
+        assert len(final_marking) == 1, \
+            'Final marking contains {} places'.format(len(final_marking))
+
+        assert len(net.places) == 4
+        assert len(net.transitions) == 5
+
+        places = ['i', 'o', '({a, b}, {c})', '({c}, {d, e})']
+        trans = ['a', 'b', 'c', 'd', 'e']
+
+        p_to_t_df, t_to_p_df = net.get_transition_relation_dfs()
+
+        p_to_t_df = p_to_t_df.reindex(trans)[places]
+        t_to_p_df = t_to_p_df.reindex(trans)[places]
+
+        assert isinstance(p_to_t_df, pd.DataFrame)
+        assert isinstance(t_to_p_df, pd.DataFrame)
+
+        assert p_to_t_df.shape == (5, 4)
+        assert t_to_p_df.shape == (5, 4)
+
+        expected_p_to_t_vals = np.asarray([
+            (1, 0, 0, 0),
+            (1, 0, 0, 0),
+            (0, 0, 1, 0),
+            (0, 0, 0, 1),
+            (0, 0, 0, 1)
+        ])
+
+        expected_t_to_p_vals = np.asarray([
+            (0, 0, 1, 0),
+            (0, 0, 1, 0),
+            (0, 0, 0, 1),
+            (0, 1, 0, 0),
+            (0, 1, 0, 0)
+        ])
+
+        assert (p_to_t_df.values == expected_p_to_t_vals).all()
+        assert (t_to_p_df.values == expected_t_to_p_vals).all()
