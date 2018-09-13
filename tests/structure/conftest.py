64513ae0c7dff763c22177ee69f727e263b1820d
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-13 02:53:23 -0300

    Refactor simple log table fixture to conftest

--- tests/structure/conftest.py
+++ tests/structure/conftest.py
@@ -11,8 +11,56 @@ __email__ = "walee@uc.cl"
 import pytest
 import pandas as pd
 import numpy as np
+import itertools as itrl
 
 from podspy.structure import CausalMatrix
+from podspy.log import constants as cnst
+from podspy.log import table as tb
+
+
+@pytest.fixture()
+def simple_log_table():
+    # Contains the following traces (with frequency)
+    # 45 <a, c, d>
+    # 42 <b, c, d>
+    # 38 <a, c, e>
+    # 22 <b, c, e>
+    t0 = ['a', 'c', 'd']
+    t1 = ['b', 'c', 'd']
+    t2 = ['a', 'c', 'e']
+    t3 = ['b', 'c', 'e']
+
+    traces = [t0, t1, t2, t3]
+
+    freq0 = 45
+    freq1 = 42
+    freq2 = 38
+    freq3 = 22
+
+    freqs = [freq0, freq1, freq2, freq3]
+
+    trace_freq_list = zip(traces, freqs)
+
+    caseid = 0
+    event_rows = []
+
+    for trace, freq in trace_freq_list:
+        for i in range(freq):
+            # repeat the caseid id over all events of a trace
+            caseids = itrl.repeat(caseid, len(trace))
+
+            event_row = zip(caseids, trace)
+            event_rows += list(event_row)
+
+            caseid += 1
+
+    labels = [cnst.CASEID, cnst.ACTIVITY]
+    event_df = pd.DataFrame.from_records(event_rows, columns=labels)
+
+    logtable = tb.LogTable()
+    logtable.event_df = event_df
+
+    return logtable
 
 
 @pytest.fixture()
--- tests/structure/test_causal.py
+++ tests/structure/test_causal.py
@@ -17,56 +17,6 @@ from podspy.log import factory as fty
 from podspy.log import table as tb
 
 
-@pytest.fixture()
-def simple_log_table():
-    # Contains the following traces (with frequency)
-    # 45 <a, c, d>
-    # 42 <b, c, d>
-    # 38 <a, c, e>
-    # 22 <b, c, e>
-    t0 = ['a', 'c', 'd']
-    t1 = ['b', 'c', 'd']
-    t2 = ['a', 'c', 'e']
-    t3 = ['b', 'c', 'e']
-
-    traces = [t0, t1, t2, t3]
-
-    freq0 = 45
-    freq1 = 42
-    freq2 = 38
-    freq3 = 22
-
-    freqs = [freq0, freq1, freq2, freq3]
-
-    trace_freq_list = zip(traces, freqs)
-
-    caseid = 0
-    event_rows = []
-
-    for trace, freq in trace_freq_list:
-        for i in range(freq):
-            # repeat the caseid id over all events of a trace
-            caseids = itrs.repeat(caseid, len(trace))
-
-            event_row = zip(caseids, trace)
-            event_rows += list(event_row)
-
-            caseid += 1
-
-    labels = [cnst.CASEID, cnst.ACTIVITY]
-    event_df = pd.DataFrame.from_records(event_rows, columns=labels)
-
-    logtable = tb.LogTable()
-    logtable.event_df = event_df
-
-    return logtable
-
-
-def test_simple_log_table_fixture(simple_log_table):
-    assert isinstance(simple_log_table, tb.LogTable)
-    print(simple_log_table.event_df.head())
-
-
 class TestCausalMatrix:
     def test_build_from_logtable(self, simple_log_table):
         # sort activities
