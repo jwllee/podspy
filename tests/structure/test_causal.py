680072509cf6edd3c67f7f99d1e5c5fca6ed922d
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-13 03:35:06 -0300

    Tested with one and two loop test fixtures

--- tests/structure/test_causal.py
+++ tests/structure/test_causal.py
@@ -5,16 +5,10 @@
 """
 
 
-import pytest
-import functools as fcts
-import itertools as itrs
 import pandas as pd
 import numpy as np
 
 from podspy.structure import CausalMatrix
-from podspy.log import constants as cnst
-from podspy.log import factory as fty
-from podspy.log import table as tb
 
 
 class TestCausalMatrix:
@@ -38,6 +32,52 @@ class TestCausalMatrix:
             (0, 0, 0, 87, 60), # c row
             (0, 0, 0, 0, 0),   # d row
             (0, 0, 0, 0, 0)    # e row
+        ]
+        mat = np.array(mat)
+
+        assert isinstance(cmat.matrix, pd.DataFrame)
+        assert (cmat.matrix.values == mat).all()
+
+    def test_build_from_logtable_one_loop(self, one_loop_log_table):
+        cmat = CausalMatrix.build_from_logtable(one_loop_log_table, sort=True)
+
+        assert isinstance(cmat, CausalMatrix)
+
+        assert cmat.activity_list == ['a', 'x', 'y']
+
+        # expected causal matrix
+        # 45 <x, a, y>
+        # 42 <x, a, a, y>
+        # 38 <x, a, a, a, y>
+
+        mat = [
+            (118, 0, 125),   # a row
+            (125, 0, 0),     # x row
+            (0, 0, 0)        # y row
+        ]
+        mat = np.array(mat)
+
+        assert isinstance(cmat.matrix, pd.DataFrame)
+        assert (cmat.matrix.values == mat).all()
+
+    def test_build_from_logtable_two_loop(self, two_loop_log_table):
+        cmat = CausalMatrix.build_from_logtable(two_loop_log_table, sort=True)
+
+        assert isinstance(cmat, CausalMatrix)
+
+        assert cmat.activity_list == ['a', 'b', 'x', 'y']
+
+        # expected causal matrix
+        # 45 <x, a, y>
+        # 42 <x, a, b, y>
+        # 38 <x, a, b, a, y>
+        # 22 <x, a, b, a, b, y>
+
+        mat = [
+            (0, 124, 0, 83), # a row
+            (60, 0, 0, 64),  # b row
+            (147, 0, 0, 0),  # x row
+            (0, 0, 0, 0)     # y row
         ]
         mat = np.array(mat)
 
