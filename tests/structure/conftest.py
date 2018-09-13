e76e0b4bc06c0b58e717c4bf585c063a55b0cad5
jonathan <jonathan.wailam.lee@gmail.com>
2018-09-13 03:34:52 -0300

    Added one loop and two loop test fixtures

--- tests/structure/conftest.py
+++ tests/structure/conftest.py
@@ -18,27 +18,7 @@ from podspy.log import constants as cnst
 from podspy.log import table as tb
 
 
-@pytest.fixture()
-def simple_log_table():
-    # Contains the following traces (with frequency)
-    # 45 <a, c, d>
-    # 42 <b, c, d>
-    # 38 <a, c, e>
-    # 22 <b, c, e>
-    t0 = ['a', 'c', 'd']
-    t1 = ['b', 'c', 'd']
-    t2 = ['a', 'c', 'e']
-    t3 = ['b', 'c', 'e']
-
-    traces = [t0, t1, t2, t3]
-
-    freq0 = 45
-    freq1 = 42
-    freq2 = 38
-    freq3 = 22
-
-    freqs = [freq0, freq1, freq2, freq3]
-
+def make_log_table(traces, freqs):
     trace_freq_list = zip(traces, freqs)
 
     caseid = 0
@@ -64,6 +44,31 @@ def simple_log_table():
 
 
 @pytest.fixture()
+def simple_log_table():
+    # Contains the following traces (with frequency)
+    # 45 <a, c, d>
+    # 42 <b, c, d>
+    # 38 <a, c, e>
+    # 22 <b, c, e>
+    t0 = ['a', 'c', 'd']
+    t1 = ['b', 'c', 'd']
+    t2 = ['a', 'c', 'e']
+    t3 = ['b', 'c', 'e']
+
+    traces = [t0, t1, t2, t3]
+
+    freq0 = 45
+    freq1 = 42
+    freq2 = 38
+    freq3 = 22
+
+    freqs = [freq0, freq1, freq2, freq3]
+    logtable = make_log_table(traces, freqs)
+
+    return logtable
+
+
+@pytest.fixture()
 def simple_causal_matrix():
     # causal matrix
     mat = [
@@ -80,3 +85,58 @@ def simple_causal_matrix():
     cmat = CausalMatrix(activity_list, df)
 
     return cmat
+
+
+@pytest.fixture()
+def one_loop_log_table():
+    # event log containing a single looping activity behavior,
+    # this is challenging for the classic alpha miner
+
+    # Contains the following traces (with frequency)
+    # 45 <x, a, y>
+    # 42 <x, a, a, y>
+    # 38 <x, a, a, a, y>
+
+    t0 = ['x', 'a', 'y']
+    t1 = ['x', 'a', 'a', 'y']
+    t2 = ['x', 'a', 'a', 'a', 'y']
+
+    traces = [t0, t1, t2]
+
+    freq0 = 45
+    freq1 = 42
+    freq2 = 38
+
+    freqs = [freq0, freq1, freq2]
+    logtable = make_log_table(traces, freqs)
+
+    return logtable
+
+
+@pytest.fixture()
+def two_loop_log_table():
+    # event log containing two-length loops behavior,
+    # this is challenging for the classic alpha miner
+
+    # Contains the following traces (with frequency)
+    # 45 <x, a, y>
+    # 42 <x, a, b, y>
+    # 38 <x, a, b, a, y>
+    # 22 <x, a, b, a, b, y>
+
+    t0 = ['x', 'a', 'y']
+    t1 = ['x', 'a', 'b', 'y']
+    t2 = ['x', 'a', 'b', 'a', 'y']
+    t3 = ['x', 'a', 'b', 'a', 'b', 'y']
+
+    traces = [t0, t1, t2, t3]
+
+    freq0 = 45
+    freq1 = 42
+    freq2 = 38
+    freq3 = 22
+
+    freqs = [freq0, freq1, freq2, freq3]
+    logtable = make_log_table(traces, freqs)
+
+    return logtable
